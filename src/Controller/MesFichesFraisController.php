beab73e39ad2ba9eaa1a438dd81c3db3026d2c04
hammoudi.dalil <dalil74@hotmail.fr>
2023-03-28 09:27:25 +0200

    V1

--- src/Controller/MesFichesFraisController.php
+++ src/Controller/MesFichesFraisController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\FicheFrais;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class MesFichesFraisController extends AbstractController
 {
     #[Route('/mes/fiches/frais', name: 'app_mes_fiches_frais')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $user = $this->getUser();
+
+        $repository = $doctrine->getRepository(FicheFrais::class);
+        $fiches = $repository->findBy(['user'=> $user]);
+        dd($fiches);
+
         return $this->render('mes_fiches_frais/index.html.twig', [
             'controller_name' => 'MesFichesFraisController',
         ]);
